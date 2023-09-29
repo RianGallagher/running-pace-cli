56001939d479bd432a6bace2eec79b7dfea18e28
Rian Gallagher <rianogallagher@gmail.com>
2023-09-29 08:35:34 +0100

    Add distance + time -> minutes/km conversion

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
-const kilometersPerHour = process.argv[2];
-const minutesPerKilometer = 60 / kilometersPerHour;
-const seconds = (minutesPerKilometer % 1) * 60;
+import inquirer from "inquirer";
+
+const options = ["km/h -> min/km", "Distance and time -> min/km"];
 
 function padNumber(number) {
-    return number.toString().padStart(2, "0");
+  return number.toString().padStart(2, "0");
 }
 
-console.log(
+/**
+ * Ask the user which application they want to run.
+ */
+async function whichConversion() {
+  const answers = await inquirer.prompt({
+    name: "conversion_type",
+    type: "list",
+    message: "Which conversion do you want to run?",
+    choices: options,
+  });
+
+  return answers.conversion_type;
+}
+
+const conversionType = await whichConversion();
+
+switch (conversionType) {
+  case "km/h -> min/km":
+    kmPerHourConversion();
+    break;
+  case "Distance and time -> min/km":
+    distanceTimeConversion();
+}
+
+async function kmPerHourConversion() {
+  const answers = await inquirer.prompt([
+    {
+      name: "kilometersPerHour",
+      type: "number",
+      message: "Kilometers per hour",
+    },
+  ]);
+  const kilometersPerHour = answers.kilometersPerHour;
+  const minutesPerKilometer = 60 / kilometersPerHour;
+  const seconds = (minutesPerKilometer % 1) * 60;
+
+  console.log(
     `${kilometersPerHour}km/h is ${padNumber(
-        Math.trunc(minutesPerKilometer)
+      Math.trunc(minutesPerKilometer)
     )}:${padNumber(Math.round(seconds))}/km`
-);
+  );
+}
+
+async function distanceTimeConversion() {
+  const answers = await inquirer.prompt([
+    {
+      name: "distance",
+      type: "number",
+      message: "Distance",
+    },
+    {
+      name: "hours",
+      type: "number",
+      message: "Hours",
+    },
+    {
+      name: "minutes",
+      type: "number",
+      message: "Minutes",
+    },
+    {
+      name: "seconds",
+      type: "number",
+      message: "Seconds",
+    },
+  ]);
+
+  const { distance, hours, minutes, seconds } = answers;
+  const totalMinutes = hours * 60 + minutes + seconds / 60;
+  const minutesPerKilometer = totalMinutes / distance;
+  const leftoverSeconds = (minutesPerKilometer % 1) * 60;
+
+  console.log(
+    `${distance}km in ${padNumber(hours)}:${padNumber(minutes)}:${padNumber(
+      seconds
+    )} is ${Math.floor(minutesPerKilometer)}:${Math.floor(leftoverSeconds)}/km`
+  );
+}
